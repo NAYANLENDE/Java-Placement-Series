33361cc3ee5db0a9f79ba26a207c9b59ed138fe6
Nayan <nayanlende@gmail.com>
2024-12-23 14:13:30 +0530

    new

--- Arrays/Largest_Element.java
+++ Arrays/Largest_Element.java
@@ -2,18 +2,18 @@ package Arrays;
 import java.util.*;
 public class Largest_Element {
     //Brute Force Approach- T.C.=O(N*log(N)) and S.C.=O(n)
-    public static void main(String [] args){
-        int arr1[] = {1,4,2,5,3};
-        System.out.println("The largest Element in the array is " + sort(arr1));
+    // public static void main(String [] args){
+    //     int arr1[] = {1,4,2,5,3};
+    //     System.out.println("The largest Element in the array is " + sort(arr1));
 
-        int arr2[] = {8,9,20,3,5};
-        System.out.println("The largest Element in the array is " + sort(arr2));
-    }
+    //     int arr2[] = {8,9,20,3,5};
+    //     System.out.println("The largest Element in the array is " + sort(arr2));
+    // }
 
-    static int sort(int arr[]){
-        Arrays.sort(arr);
-        return arr[arr.length - 1];
-    } 
+    // static int sort(int arr[]){
+    //     Arrays.sort(arr);
+    //     return arr[arr.length - 1];
+    // } 
 
 
     //Recursive Approach T.C.=O(N) and S.C.=O(1)
