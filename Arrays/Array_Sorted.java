374057069db2e0d54973112d7556382b56887b21
Nayan <nayanlende@gmail.com>
2025-01-06 10:11:25 +0530

    Added

--- Arrays/Array_Sorted.java
+++ Arrays/Array_Sorted.java
@@ -1,6 +1,6 @@
 package Arrays;
 
-public class Array_Sorted {
+class Array_Sorted {
     //Brute Force Approach T.C.= O(N^2) S.C. = O(1)
 
     static boolean isSorted(int arr[], int n){
@@ -13,8 +13,20 @@ public class Array_Sorted {
         return true;
     }
 
+
+    //Optimal Approach  T.C. = O(N)  S.C. = O(1)
+
+    static boolean isSorted(int arr[], int n){
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < arr[i-1])
+                return false;
+            }
+            return true;
+        }
+            
+
     public static void main(String [] args){
-        int arr[] = {1, 2, 3, 9, 5}, n = 5;
+        int arr[] = {1, 2, 3, 4, 5}, n = 5;
 
         System.out.println(isSorted(arr, n));
     }
